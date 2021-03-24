fae0ed9728c51cee64e2ed92387343b89331b568
EinavY <80457267+EinavY@users.noreply.github.com>
2021-03-25 00:17:06 +0200

    Add files via upload

---
+++ Targil3.py
@@ -0,0 +1,24 @@
+def revword(word:str) -> str:
+    word = word.lower()
+    t = word[::-1]
+    return t
+def countword()->int:
+    txt = open('test.txt', 'r')
+    count = 0
+    i = 0
+    for line in txt:
+        if i == 0:
+            word = line.rstrip("\n")
+            print (word)
+            count =+1
+            i =+1
+        else:
+            line = line.split()
+            for ii in line:
+                z = revword (ii)
+                if z == word:
+                    count = count + 1
+                    
+
+    return count
+print(countword())
